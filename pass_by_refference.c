d414f83de6c68543fdff472154b71ebf7b45ea43
sagarpd28 <sagarpd28@gmail.com>
2025-11-26 21:23:20 +0530

    vv

--- pass_by_refference.c
+++ pass_by_refference.c
@@ -1,25 +1,25 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// void calculate(int *x, int *y, int *sum) {
-//     *sum = *x + *y;
-// }
+void calculate(int *x, int *y, int *sum) {
+    *sum = *x + *y;
+}
 
-// int main() {
+int main() {
 
-//     int a, b, result;
+    int a, b, result;
 
-//     printf("Enter first number: ");
-//     scanf("%d", &a);
+    printf("Enter first number: ");
+    scanf("%d", &a);
 
-//     printf("Enter second number: ");
-//     scanf("%d", &b);
+    printf("Enter second number: ");
+    scanf("%d", &b);
 
-//     calculate(&a, &b, &result);
+    calculate(&a, &b, &result);
 
-//     printf("The sum of %d and %d is: %d\n", a, b, result);
+    printf("The sum of %d and %d is: %d\n", a, b, result);
 
-//     return 0;
-// }
+    return 0;
+}
 
 void calculate(int *x, int *y, int *sum){
     *sum = *x + *y;
